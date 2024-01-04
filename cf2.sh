ef4e1f78ac2efe10a95eef33446dc8b0d578be12
free net <74614880+arismaramar@users.noreply.github.com>
2024-01-05 03:44:40 +0700

    Update cf2.sh

--- cf2.sh
+++ cf2.sh
@@ -19,7 +19,7 @@ red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 clear
 
 function get_acme_domain() {
-    vpnpro=$(cat /etc/xray/domain)
+    anggunre=$(cat /etc/xray/domain)
     clear
     echo -e " ┌─────────────────────────────────────────────────────────┐"
     echo -e "─│                        ${CYAN}WELCOME TO${NC}                       │─"
@@ -41,20 +41,20 @@ function get_acme_domain() {
     echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Sedang mendownload sertifikat kedalam VPS${NC}"
     /root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
     /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
-    /root/.acme.sh/acme.sh --issue -d $vpnpro --standalone -k ec-256 >/dev/null 2>&1
-    ~/.acme.sh/acme.sh --installcert -d $vpnpro --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc >/dev/null 2>&1
+    /root/.acme.sh/acme.sh --issue -d $anggunre --standalone -k ec-256 >/dev/null 2>&1
+    ~/.acme.sh/acme.sh --installcert -d $anggunre --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc >/dev/null 2>&1
     rm /etc/haproxy/yha.pem >/dev/null 2>&1
     cat /etc/xray/xray.crt /etc/xray/xray.key | tee >/dev/null 2>&1
     echo -e "   [${GREEN}DONE${NC}] ${CYAN}Pembaruan Sertifikat Selesai${NC}"
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/nginx/conf.d/xray.conf >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /var/www/html/index.html >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/tcp.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/udp.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/ssl.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/ws-ssl.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /var/www/html/index.html >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/nginx/conf.d/xray.conf >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /var/www/html/index.html >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/openvpn/tcp.ovpn >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/openvpn/udp.ovpn >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/openvpn/ssl.ovpn >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /etc/openvpn/ws-ssl.ovpn >/dev/null 2>&1
+    sed -i "s/${LAST_DOMAIN}/${anggunre}/g" /var/www/html/index.html >/dev/null 2>&1
     sleep 2
     echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart Daemon Reload Service${NC}"
     systemctl daemon-reload >/dev/null 2>&1
@@ -85,7 +85,7 @@ ns_domain_cloudflare() {
     sleep 3
     sed -i "s/$NS/$NSVPSKU/g" /etc/systemd/system/client.service >/dev/null 2>&1
     sed -i "s/$NS/$NSVPSKU/g" /etc/systemd/system/server.service >/dev/null 2>&1
-    echo -e "   [${ORANGE}DONE${NC}] ${CYAN}Domain kamu sekarang${NC} [${ORANGE}$vpnpro${NC}]"
+    echo -e "   [${ORANGE}DONE${NC}] ${CYAN}Domain kamu sekarang${NC} [${ORANGE}$anggunre${NC}]"
     sleep 2
     echo -e "   [${ORANGE}DONE${NC}] ${CYAN}NS-Domain kamu sekarang${NC} [${ORANGE}$NSVPSKU${NC}]"
     sleep 2
