da5bc66064d4047c74a2ff248031921410bdeaef
free net <74614880+arismaramar@users.noreply.github.com>
2024-01-05 06:19:22 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,81 +1,81 @@
 #!/bin/bash
-# // font color configuration
-LAST_DOMAIN="$(cat /etc/xray/domain)"
-NS="$(cat /etc/xray/dns)"
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-clear
+DOMAIN=vpnpro.tech
+sub=$(tr </dev/urandom -dc a-z0-9 | head -c4)
+SUB_DOMAIN=${sub}.vpnpro.tech
+CF_ID=arismar.amar@gmail.com
+CF_KEY=88ecae78b53455a919ccecd22bdbd0332f7c7
+set -euo pipefail
+IP=$(wget -qO- ipinfo.io/ip)
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
 
-function get_acme_domain() {
-    vpnpro=$(cat /etc/xray/domain)
-    clear
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Proses sedang berlangsung${NC} "
-    systemctl stop nginx
-    systemctl stop haproxy
-	systemctl stop xray
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Memperbarui semua sertifikat${NC}"
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Sedang mendownload sertifikat kedalam VPS${NC}"
-    /root/.acme.sh/acme.sh --upgrade --auto-upgrade >/dev/null 2>&1
-    /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt >/dev/null 2>&1
-    /root/.acme.sh/acme.sh --issue -d $vpnpro --standalone -k ec-256 >/dev/null 2>&1
-    ~/.acme.sh/acme.sh --installcert -d $vpnpro --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc >/dev/null 2>&1
-    rm /etc/haproxy/yha.pem >/dev/null 2>&1
-    cat /etc/xray/xray.crt /etc/xray/xray.key | tee >/dev/null 2>&1
-    echo -e "   [${GREEN}DONE${NC}] ${CYAN}Pembaruan Sertifikat Selesai${NC}"
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/nginx/conf.d/xray.conf >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /var/www/html/index.html >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/tcp.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/udp.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/ssl.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /etc/openvpn/ws-ssl.ovpn >/dev/null 2>&1
-    sed -i "s/${LAST_DOMAIN}/${vpnpro}/g" /var/www/html/index.html >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart Daemon Reload Service${NC}"
-    systemctl daemon-reload >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart SlowDns Server Service${NC}"
-    systemctl restart server >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart SlowDns Client Service${NC}"
-    systemctl restart client >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart Haproxy Loadbalance${NC}"
-    systemctl restart haproxy >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart Nginx WebServer${NC}"
-    systemctl restart nginx >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Restart Xray Service${NC}"
-    systemctl restart xray >/dev/null 2>&1
-    sleep 2
-    echo -e "   [${GREEN}DONE${NC}] ${CYAN}Ganti Domain dan Restart Service Selesai"
-    sleep 2
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Proses pointing NS-Domain${NC} "
-    sleep 3
-}
-ns_domain_cloudflare() {
-    wget https://raw.githubusercontent.com/arismaramar/sldns/main/cfnvpnpro.sh>/dev/null 2>&1 && chmod +x cfnvpnpro.sh && ./cfnvpnpro.sh >/dev/null 2>&1
-    NSVPSKU=$(cat /etc/xray/dns)
-    sleep 3
-    sed -i "s/$NS/$NSVPSKU/g" /etc/systemd/system/client.service >/dev/null 2>&1
-    sed -i "s/$NS/$NSVPSKU/g" /etc/systemd/system/server.service >/dev/null 2>&1
-    echo -e "   [${ORANGE}DONE${NC}] ${CYAN}Domain kamu sekarang${NC} [${ORANGE}$vpnpro${NC}]"
-    sleep 2
-    echo -e "   [${ORANGE}DONE${NC}] ${CYAN}NS-Domain kamu sekarang${NC} [${ORANGE}$NSVPSKU${NC}]"
-    sleep 2
-    echo -e "─────────────────────────────────────────────────────────────"
-    read -n 2 -s -r -p "Tekan sembarang untuk kembali ke menu"
-    menu
-}
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
 
-cloudflare() {
-    echo -e "   [${ORANGE}INFO${NC}] ${CYAN}Proses Pointing Sedang Berlangsung${NC} "
-    wget https://raw.githubusercontent.com/arismaramar/sldns/main/cfdvpnpro.sh  >/dev/null 2>&1 && chmod +x cfdvpnpro.sh && ./cfdvpnpro.sh >/dev/null 2>&1
-    get_acme_domain
-    ns_domain_cloudflare
-}
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}' | jq -r .result.id)
+fi
 
-clear
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}')
+echo $SUB_DOMAIN >/etc/xray/domain
+echo "Subdomain kamu adalah : $SUB_DOMAIN"
+sleep 3
+rm -f /root/cfdvpnpro.sh
+
+
+DOMAIN="vpnpro.tech"
+DAOMIN=$(cat /etc/xray/domain)
+SUB=$(tr </dev/urandom -dc a-z0-9 | head -c4)
+SUB_DOMAIN=${SUB}."vpnpro.tech"
+NS_DOMAIN=ns.${SUB_DOMAIN}
+CF_ID=arismar.amar@gmail.com
+CF_KEY=88ecae78b53455a919ccecd22bdbd0332f7c7
+set -euo pipefail
+IP=$(wget -qO- ipinfo.io/ip)
+echo "Updating DNS NS for ${NS_DOMAIN}..."
+ZONE=$(
+	curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+	-H "X-Auth-Email: ${CF_ID}" \
+	-H "X-Auth-Key: ${CF_KEY}" \
+	-H "Content-Type: application/json" | jq -r .result[0].id
+)
+
+RECORD=$(
+	curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
+	-H "X-Auth-Email: ${CF_ID}" \
+	-H "X-Auth-Key: ${CF_KEY}" \
+	-H "Content-Type: application/json" | jq -r .result[0].id
+)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+	RECORD=$(
+		curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+		-H "X-Auth-Email: ${CF_ID}" \
+		-H "X-Auth-Key: ${CF_KEY}" \
+		-H "Content-Type: application/json" \
+		--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DAOMIN}'","proxied":false}' | jq -r .result.id
+	)
+fi
+
+RESULT=$(
+	curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+	-H "X-Auth-Email: ${CF_ID}" \
+	-H "X-Auth-Key: ${CF_KEY}" \
+	-H "Content-Type: application/json" \
+	--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DAOMIN}'","proxied":false}'
+)
+echo $NS_DOMAIN >/etc/xray/dns
+rm -f /root/cfnvpnpro.sh
