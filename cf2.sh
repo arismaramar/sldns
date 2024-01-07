99f0a994a7faf998621a6fba65570405cfbce2fa
free net <74614880+arismaramar@users.noreply.github.com>
2024-01-07 17:19:14 +0700

    Update cf2.sh

--- cf2.sh
+++ cf2.sh
@@ -1,82 +1,27 @@
 #!/bin/bash
-DOMAIN=anggunre.shop
-sub=$(tr </dev/urandom -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.anggunre.shop
-CF_ID=arismar.amar@gmail.com
-CF_KEY=88ecae78b53455a919ccecd22bdbd0332f7c7
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip)
-echo "Updating DNS for ${SUB_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}')
-echo $SUB_DOMAIN >/etc/xray/domain
-echo "Subdomain kamu adalah : $SUB_DOMAIN"
+# // font color configuration
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+Font="\033[0m"
+gray="\e[1;30m"
+total_ram=$(grep "MemTotal: " /proc/meminfo | awk '{ print $2}')
+totalram=$(($total_ram / 1024))
+MYIP=$(curl -sS ipv4.icanhazip.com)
+SUB_DOMAIN="$(cat /etc/xray/domain)"
+NS="$(cat /etc/xray/dns)"
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+clear
+echo -e "   [${ORANGE}INFO${NC}] ${CYAN}ponting ke domain anggunre.shop berlangsung${NC} "
+wget -O cfdanggunre.sh https://raw.githubusercontent.com/arismaramar/sldns/main/cfdanggunre.sh >/dev/null 2>&1 && chmod +x cfdanggunre.sh && ./cfdanggunre.sh >/dev/null 2>&1
+ sleep 3
+wget -O cfnanggunre.sh  https://raw.githubusercontent.com/arismaramar/sldns/main/cfnanggunre.sh >/dev/null 2>&1 && chmod +x cfnanggunre.sh && ./cfnanggunre.sh >/dev/null 2>&1
 sleep 3
-rm -f /root/cfanggunre.sh
 
-#!/bin/bash
-DOMAIN="anggunre.shop"
-DAOMIN=$(cat /etc/xray/domain)
-SUB=$(tr </dev/urandom -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${SUB}."anggunre.shop"
-NS_DOMAIN=ns.${SUB_DOMAIN}
-CF_ID=arismar.amar@gmail.com
-CF_KEY=88ecae78b53455a919ccecd22bdbd0332f7c7
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip)
-echo "Updating DNS NS for ${NS_DOMAIN}..."
-ZONE=$(
-	curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-	-H "X-Auth-Email: ${CF_ID}" \
-	-H "X-Auth-Key: ${CF_KEY}" \
-	-H "Content-Type: application/json" | jq -r .result[0].id
-)
-
-RECORD=$(
-	curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${NS_DOMAIN}" \
-	-H "X-Auth-Email: ${CF_ID}" \
-	-H "X-Auth-Key: ${CF_KEY}" \
-	-H "Content-Type: application/json" | jq -r .result[0].id
-)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-	RECORD=$(
-		curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-		-H "X-Auth-Email: ${CF_ID}" \
-		-H "X-Auth-Key: ${CF_KEY}" \
-		-H "Content-Type: application/json" \
-		--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DAOMIN}'","proxied":false}' | jq -r .result.id
-	)
-fi
-
-RESULT=$(
-	curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-	-H "X-Auth-Email: ${CF_ID}" \
-	-H "X-Auth-Key: ${CF_KEY}" \
-	-H "Content-Type: application/json" \
-	--data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${DAOMIN}'","proxied":false}'
-)
-echo $NS_DOMAIN >/etc/xray/dns
-echo "dns kamu adalah : $NS_DOMAIN"
-rm -f /root/cfnanggunre.sh
+echo "Subdomain kamu adalah : $SUB_DOMAIN"
+echo "NS_DOMAIN kamu adalah : $NS"
