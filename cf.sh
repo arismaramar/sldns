9b8f282698bce090cad2f5d76dc80de2ae8103ab
free net <74614880+arismaramar@users.noreply.github.com>
2024-01-07 17:04:44 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-cf() {
+
     echo -e "   [${ORANGE}INFO${NC}] ${CYAN}ponting ke vpnpro.tech berlangsung${NC} "
     wget https://raw.githubusercontent.com/arismaramar/sldns/main/cfdvpnpro.sh >/dev/null 2>&1 && chmod +x cfdvpnpro.sh && ./cfdvpnpro.sh >/dev/null 2>&1
-    ns_domain_cloudflare
    
-}
-
-ns_domain_cloudflare() {
     wget https://raw.githubusercontent.com/arismaramar/sldns/main/cfnvpnpro.sh >/dev/null 2>&1 && chmod +x cfnvpnpro.sh && ./cfnvpnpro.sh >/dev/null 2>&1
 
 
